914591ce89b73e576b7b178b05e2d2fe0d3f3729
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-09-13 14:05:06 +0900

    #S05P21B101-288 feat: 검증 entity

--- backend/src/main/java/com/b101/recruit/domain/entity/Verification.java
+++ backend/src/main/java/com/b101/recruit/domain/entity/Verification.java
@@ -1,4 +1,32 @@
 package com.b101.recruit.domain.entity;
 
-public class Verification {
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 검증 모델 정의.
+ */
+
+@Entity
+@Getter
+@Setter
+public class Verification extends BaseEntity {
+	
+	private PersonalInfo personalinfo; // 신상정보 id
+	private Date add_time; // 등록일
+	private String currentstatus; // 현재상태(대기중, 승인완료, 거절)
+	
+	@Column(columnDefinition = "LONGTEXT")
+	private String reasonsrejection; // 반려사유
+	
+	@PrePersist
+	public void createdAt() {
+		this.add_time = new Date();
+	}
 }
