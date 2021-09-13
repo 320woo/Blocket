f09d05749c104760b0ae13ad4c9b9ad2a29e095c
안창호 <aysul0@naver.com>
2021-09-13 15:21:36 +0900

    #S05P21B101-304 feat : [안창호] 회원 DB 설계 - JPA

--- backend/src/main/java/com/b101/recruit/domain/entity/User.java
+++ backend/src/main/java/com/b101/recruit/domain/entity/User.java
@@ -1,4 +1,36 @@
 package com.b101.recruit.domain.entity;
 
-public class User {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int type; // 0: 회원, 1: 기업
+
+    private String belong; // 소속
+
+    private String phoneNumber;
+
+    private int withdrawal; // 탈퇴 여부
+
+    private int brn; // 사업자등록 번호
 }
