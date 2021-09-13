d3068825e7dc8558f33c4491e3869422a9f92fce
안창호 <aysul0@naver.com>
2021-09-13 15:26:45 +0900

    #S05P21B101-303 feat : [안창호] 회원_지원내역 DB 설계 - JPA

--- backend/src/main/java/com/b101/recruit/domain/entity/UserRecruit.java
+++ backend/src/main/java/com/b101/recruit/domain/entity/UserRecruit.java
@@ -1,4 +1,25 @@
 package com.b101.recruit.domain.entity;
 
-public class UserRecruit {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserRecruit extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "job_posting_id")
+    private JobPosting jobPosting;
 }
