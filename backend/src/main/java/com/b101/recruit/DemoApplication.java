115fa78c78f2c82476ec3d26b33f3eab7c5ac0fe
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-09-15 14:54:13 +0900

    #S05P21B101-296 feat: 인코딩 설정

--- backend/src/main/java/com/b101/recruit/DemoApplication.java
+++ backend/src/main/java/com/b101/recruit/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.b101.recruit;
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	//http post 통신시에 utf-8 인코딩 설정
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    //utf-8 인코딩 맞춰주는 필터
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+    
 }
