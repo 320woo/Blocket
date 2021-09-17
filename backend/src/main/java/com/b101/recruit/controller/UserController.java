3f6bf89c51aad267429a674e6ca55121f5fbf39b
정세원 <eeych@naver.com>
2021-09-17 10:38:56 +0900

    #S05P21B101-343 feat : 회원가입 API 구현

--- backend/src/main/java/com/b101/recruit/controller/UserController.java
+++ backend/src/main/java/com/b101/recruit/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.b101.common.model.response.BaseResponseBody;
 import com.b101.common.util.JwtTokenUtil;
 import com.b101.recruit.domain.entity.User;
 import com.b101.recruit.reponse.UserLoginPostRes;
 import com.b101.recruit.request.UserLoginPostReq;
+import com.b101.recruit.request.UserRegisterPostReq;
 import com.b101.recruit.service.impl.UserService;
 
 import io.swagger.annotations.Api;
@@ -24,24 +26,40 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/recruit/users")
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
+	// 회원 가입
+	@PostMapping("/register")
+	@ApiOperation(value = "회원가입", notes = "사용자의 정보를 입력하여 회원가입한다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "success"), @ApiResponse(code = 401, message = "fail"),
+			@ApiResponse(code = 404, message = "url error"), @ApiResponse(code = 500, message = "server error") })
+	public ResponseEntity<? extends BaseResponseBody> register(@RequestBody UserRegisterPostReq registerInfo) {
+		try {
+			userService.createUser(registerInfo);
+			return ResponseEntity.status(200).body(BaseResponseBody.of(200, "회원가입에 성공하였습니다."));
+		} catch (Exception e) {
+			return ResponseEntity.status(401).body(BaseResponseBody.of(401, "회원가입에 실패하였습니다."));
+		}
+	}
+
 	@PostMapping("/login")
-	@ApiOperation(value="로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
-	@ApiResponses({ @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class)})
-	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value= "로그인 정보",required = true) UserLoginPostReq loginInfo){
+	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
+	@ApiResponses({ @ApiResponse(code = 200, message = "로그인 성공", response = UserLoginPostRes.class) })
+	public ResponseEntity<UserLoginPostRes> login(
+			@RequestBody @ApiParam(value = "로그인 정보", required = true) UserLoginPostReq loginInfo) {
 		String userId = loginInfo.getEmail();
 		String password = loginInfo.getPassword();
-		
+
 		User user = userService.findByUserId(userId);
-		if(user!=null) {
+		System.out.println(user.getEmail());
+		if (user != null) {
 //			String passTmp = passwordEncoder.encode(user.getPassword());
-			if(passwordEncoder.matches(password, user.getPassword())) {
+			if (passwordEncoder.matches(password, user.getPassword())) {
 				return ResponseEntity.ok(UserLoginPostRes.of(200, "로그인에 성공하였습니다.", JwtTokenUtil.createToken(userId)));
 			}
 		}
