8ef57c3a88bc872c30d005c4bc72073759ae5bd6
tofan0412 <tofan12312@gmail.com>
2021-09-17 13:52:18 +0900

    #S05P21B101-289 feat : 구조작성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,12 +6,20 @@
   <router-view />
 </template>
 
+<script>
+
+export default {
+  setup() {
+    
+  },
+}
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
