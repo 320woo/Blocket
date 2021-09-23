21a5c4c3566ee802d0d76f950400dfcf5664c012
정세원 <eeych@naver.com>
2021-09-23 15:14:21 +0900

    #S05P21B101-371 fix : 로그인 API 오류 수정

--- backend/src/main/java/com/b101/common/util/JwtTokenUtil.java
+++ backend/src/main/java/com/b101/common/util/JwtTokenUtil.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -20,28 +19,31 @@ import io.jsonwebtoken.UnsupportedJwtException;
 @Component
 public class JwtTokenUtil {
 
-	private static String secretKey;
-	private static Integer expirationTime;
+//	@Value("${jwt.secret}")
+	private static String secretKey = "Ss2jTeYzh2afL5404K9GWNBb3obZcrZsC8Hv0eCpObOjY88f8ZQYBoalGHirlMdY";
+//	@Value("${jwt.expiration}")
+	private static Integer expirationTime = 864000000;
 
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String ISSUER = "b101.com";
 
-	@Autowired
-	public JwtTokenUtil(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") Integer expirationTime) {
-		this.secretKey = secretKey;
-		this.expirationTime = expirationTime;
-	}
-
-	public void setExpirationTime() {
-		JwtTokenUtil.expirationTime = expirationTime;
-	}
+//	@Autowired
+//	public JwtTokenUtil(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") Integer expirationTime) {
+//		this.secretKey = secretKey;
+//		this.expirationTime = expirationTime;
+//	}
+//
+//	public void setExpirationTime() {
+//		JwtTokenUtil.expirationTime = expirationTime;
+//	}
 
 	public static String createToken(String userId) {
 		Claims claims = Jwts.claims().setSubject(userId);
 		Date now = new Date();
 		return Jwts.builder().setClaims(claims).setIssuer(ISSUER).setIssuedAt(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
-				.setExpiration(new Date(now.getTime() + expirationTime)).signWith(SignatureAlgorithm.HS256, secretKey)
+				.setExpiration(new Date(now.getTime() + expirationTime))
+				.signWith(SignatureAlgorithm.HS256, secretKey)
 				.compact();
 	}
 
