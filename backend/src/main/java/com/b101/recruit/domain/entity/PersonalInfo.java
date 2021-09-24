7d5b96291c516adc4d67d6990825461dc122b9a1
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-09-24 13:40:19 +0900

    #S05P21B101-342 fix: 신상정보 CRUD 오류 수정

--- backend/src/main/java/com/b101/recruit/domain/entity/PersonalInfo.java
+++ backend/src/main/java/com/b101/recruit/domain/entity/PersonalInfo.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import com.google.common.base.Optional;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -45,4 +47,7 @@ public class PersonalInfo extends BaseEntity {
 	private String selfIntro; 		// 간단한 자기소개
 	private String jobExperience; 	// 지원 직무와 관련된 경험
 	
+	public void setUser(User user) {
+		this.user=user;
+	}
 }
