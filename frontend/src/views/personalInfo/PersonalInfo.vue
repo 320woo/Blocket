d19aae21b043deae6155fcc5ededd9cb7a94a24e
tofan0412 <tofan12312@gmail.com>
2021-09-24 10:52:53 +0900

    #S05P21B101-226 feat : axios 설치

--- frontend/src/views/personalInfo/PersonalInfo.vue
+++ frontend/src/views/personalInfo/PersonalInfo.vue
@@ -7,16 +7,20 @@
 <InputText v-model="state.privateKey" />
 
 <h1>2. 생성한 지갑 정보 DB에 저장하기</h1>
+<Button label="DB에 저장하기" @click="saveWalletInDB" />
 
 
 <h1>3. 지갑 정보 트랜잭션 전송 통해 infura 퍼블릭 노드에 저장하기</h1>
 <Button label="Submit" @click="saveWalletInEther" />
+
+
 
 <h1>신상 정보 입력</h1>
 </template>
 <script>
 import { createWeb3 } from '@/utils/itemInventory.js'
 import { reactive } from '@vue/reactivity'
+import axios from 'axios'
 
 export default {
 name: 'personalInfo',
@@ -46,7 +50,13 @@ methods: {
 
     // 이제 생성한 지갑의 정보를 infura의 퍼블릭 노드에 저장하자.
 
-  }
+  },
+  saveWalletInDB() {
+
+  },
+  saveWalletInEther() {
+
+  },
 },
 }
 </script>
