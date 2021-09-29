ba1ba34806876ed773b65a5df1e2bc0a6365b45e
안창호 <aysul0@naver.com>
2021-09-29 13:16:40 +0900

    #S05P21B101-455 feat : [안창호] 지갑 CRUD

--- backend/src/main/java/com/b101/recruit/service/impl/UserWalletService.java
+++ backend/src/main/java/com/b101/recruit/service/impl/UserWalletService.java
@@ -1,9 +1,38 @@
 package com.b101.recruit.service.impl;
 
+import com.b101.recruit.domain.entity.User;
+import com.b101.recruit.domain.entity.UserWallet;
+import com.b101.recruit.domain.repository.JpaUserWalletRepository;
+import com.b101.recruit.domain.repository.UserRepository;
+import com.b101.recruit.domain.repository.UserWalletRepository;
+import com.b101.recruit.request.UserWalletRegisterPostReq;
 import com.b101.recruit.service.IUserWalletService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("userWalletService")
 public class UserWalletService implements IUserWalletService {
 
+    @Autowired
+    UserWalletRepository userWalletRepository;
+
+    @Autowired
+    JpaUserWalletRepository jpaUserWalletRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void createUserWallet(UserWalletRegisterPostReq userWalletRegisterPostReq) {
+        User user = userService.findByUserEmail(userWalletRegisterPostReq.getEmail());
+        UserWallet userWallet = new UserWallet();
+        userWallet.setUser(user);
+        userWallet.setAddress(userWalletRegisterPostReq.getAddress());
+        userWallet.setBalance(userWalletRegisterPostReq.getBalance());
+        userWallet.setReceiving_count(userWalletRegisterPostReq.getReceiving_count());
+        userWalletRepository.save(userWallet);
+    }
 }
