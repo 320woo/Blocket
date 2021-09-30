22f925ce6a2c552f6a775d9fba54a25c078e4254
DESKTOP-KVCQHCD\multicampus <23gywls@naver.com>
2021-09-30 23:17:04 +0900

    #S05P21B101-406 feat: 파일 예제 test

--- backend/src/main/java/com/b101/recruit/domain/repository/GalleryRepository.java
+++ backend/src/main/java/com/b101/recruit/domain/repository/GalleryRepository.java
@@ -1,5 +1,7 @@
 package com.b101.recruit.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.b101.recruit.domain.entity.Gallery;
 @Repository
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 
+	@Override
+	List<Gallery> findAll();
+	
 }
