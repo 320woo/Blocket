49164bb29662f871027f90ea2dd47c189f685472
= <=>
2021-10-08 08:20:17 +0900

    feat: [임현우] 메인 구성하기

--- frontend/src/components/main/main.vue
+++ frontend/src/components/main/main.vue
@@ -8,12 +8,18 @@
                 <div class="p-field p-col-5">
                    <br/>
                    <h1>Blocket이란?</h1>
+                   <span id="p1">
                    <p>1. 자격증, 성적증명서 등 채용 과정에서</p>
                    <p>필요한 개인 정보를 서비스 내 제출</p>
+                   </span>
+                   <span id="p2">
                    <p>2. 관리자가 데이터 검증 후 블록체인에</p>
                    <p>해시값으로 저장</p>
+                   </span>
+                   <span id="p3">
                    <p>3. 기업들은 서비스의 지원자 계정을 통해</p>
                    <p>블록체인 내에서 정보를 손쉽게 조회</p>
+                   </span>
                 </div>
             </div>
         </div>
@@ -42,6 +48,33 @@ export default {
   margin-left: 30%;
   margin-top : 5%;
   width: 50%;
+}
+
+#p1 {
+  animation-duration: 2s;
+  animation-name: slidein;
+}
+
+#p2 {
+  animation-duration: 4s;
+  animation-name: slidein;
+}
+
+#p3 {
+  animation-duration: 6s;
+  animation-name: slidein;
+}
+
+@keyframes slidein {
+  from {
+    margin-left: 400px;
+    width: 100%
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
 }
 
 </style>
